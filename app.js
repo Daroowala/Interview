091fc601b0caafa5c190acb6000fbfc3ce2b99ac
my name <blueshaperd3@gmail.com>
2024-09-05 01:54:14 +0500

    Template Literals

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // Template Literals
-let obj = {
-    name: "Rehan",
-    age: 41,
-    city: "Karachi",
-    occupation: "Web Developer",
-    hobbies: ["Coding", "Reading", "Traveling"],
-};
+// let obj = {
+//     name: "Rehan",
+//     age: 41,
+//     city: "Karachi",
+//     occupation: "Web Developer",
+//     hobbies: ["Coding", "Reading", "Traveling"],
+// };
 
-let outPut = `My name is ${obj.name} i am live in ${obj.city} and i am ${obj.occupation}`;
-console.log(outPut);
+// let outPut = `My name is ${obj.name} i am live in ${obj.city} and i am ${obj.occupation}`;
+// console.log(outPut);
